da9c4508410b3f3fbabde8e33ff92b50b8ba78f5
戴为琴 <daicai1989@126.com>
2020-03-25 10:26:44 +0800

    Initial Commit

---
+++ cloudfunctions/add/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  let { a, b } = event
+  let sum = a + b
+
+  return {
+    sum,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
